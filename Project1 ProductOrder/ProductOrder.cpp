1f4f47debbf8710cd989f471102b758dfea9e9c9
Justyn Durnford <jpdurnford@alaska.edu>
2020-09-02 13:20:55 -0700

    Started Project1: ProductOrder. It compiles!!!!! Added files for the ProductOrder class as well as a test file.

---
+++ Project1 ProductOrder/ProductOrder.cpp
@@ -0,0 +1,8 @@
+// Project1 ProductOrder
+// ProductOrder.cpp
+// Justyn Durnford
+// Created on 2020-09-02
+// Last updated on 2020-09-02
+// Source file for the ProductOrder class
+
+#include "ProductOrder.h"
---
+++ Project1 ProductOrder/ProductOrder.h
@@ -0,0 +1,13 @@
+// Project1 ProductOrder
+// ProductOrder.h
+// Justyn Durnford
+// Created on 2020-09-02
+// Last updated on 2020-09-02
+// Header file for the ProductOrder class
+
+#ifndef PRODUCTORDER_H
+#define PRODUCTORDER_H
+
+
+
+#endif // PRODUCTORDER_H
