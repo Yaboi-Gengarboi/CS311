7be896dd8a7cd5312ea7ba4c3e2e393728dad6b3
Justyn Durnford <jpdurnford@alaska.edu>
2020-09-04 22:16:21 -0700

    -The increment operators now use std::numeric_limits to prevent overflow.

---
+++ Project1 ProductOrder/productorder.cpp
@@ -0,0 +1,153 @@
+// Project1 ProductOrder
+// productorder.cpp
+// Justyn Durnford
+// Created on 2020-09-02
+// Last updated on 2020-09-04
+// Source file for the ProductOrder class
+
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
+
+#include "productorder.h"
+
+#include <limits>
+using std::numeric_limits;
+
+#include <string>
+using std::string;
+using std::to_string;
+
+#include <ostream>
+using std::ostream;
+
+void ProductOrder::setValues(const string& name, int number)
+{
+	if (_name != name)
+		_name = name;
+	if (number >= 0)
+		_number = number;
+}
+
+ProductOrder::ProductOrder()
+{
+	setValues("UNSPECIFIED", -1);
+}
+
+ProductOrder::ProductOrder(const string& name, int number)
+{
+	setValues(name, number);
+}
+
+string ProductOrder::getName() const
+{
+	return _name;
+}
+
+int ProductOrder::getNumber() const
+{
+	return _number;
+}
+
+void ProductOrder::setName(const string& name)
+{
+	setValues(name, -1);
+}
+
+void ProductOrder::setNumber(int number)
+{
+	setValues(_name, number);
+}
+
+bool ProductOrder::empty() const
+{
+	return _number == 0;
+}
+
+string ProductOrder::toString() const
+{
+	return _name + " (" + to_string(_number) + ")";
+}
+
+ProductOrder& ProductOrder::operator ++ ()
+{
+	if (_number == numeric_limits<int>::max()) // Prevents overflow
+		return *this;
+
+	++_number;
+	return *this;
+}
+
+ProductOrder ProductOrder::operator ++ (int dummy)
+{
+	ProductOrder po(*this);
+	++(*this);
+	return po;
+}
+
+ProductOrder& ProductOrder::operator -- ()
+{
+	if (_number == 0)
+		return *this;
+
+	--_number;
+	return *this;
+}
+
+ProductOrder ProductOrder::operator -- (int dummy)
+{
+	ProductOrder po(*this);
+	--(*this);
+	return po;
+}
+
+bool operator == (const ProductOrder& po1, const ProductOrder& po2)
+{
+	if (po1.getName() != po2.getName())
+		return false;
+
+	if (po1.getNumber() != po2.getNumber())
+		return false;
+
+	return true;
+}
+
+bool operator != (const ProductOrder& po1, const ProductOrder& po2)
+{
+	if (po1.getName() != po2.getName())
+		return true;
+
+	if (po1.getNumber() != po2.getNumber())
+		return true;
+
+	return false;
+}
+
+ostream& operator << (ostream& os, const ProductOrder& po)
+{
+	os << po.toString();
+	return os;
+}
--- Project1 ProductOrder/productorder.h
+++ Project1 ProductOrder/productorder.h
@@ -35,6 +35,7 @@ For more information, please refer to <http://unlicense.org/>
 #ifndef PRODUCTORDER_H
 #define PRODUCTORDER_H
 
+#include <limits>
 #include <string>
 #include <ostream>
 
@@ -42,7 +43,6 @@ For more information, please refer to <http://unlicense.org/>
 // or product you might find in a store or on a website. 
 //
 // INVARIANTS: 
-//     _name.empty() != true
 //     _number >= 0
 class ProductOrder
 {
@@ -55,7 +55,6 @@ class ProductOrder
 	// simply call this function, which will make the
 	// code more DRY.
 	// 
-	// IF name is an empty string, _name will not be changed.
 	// IF number is < 0, _number will not be changed.
 	void setValues(const std::string& name, int number);
 
@@ -68,8 +67,7 @@ class ProductOrder
 
 	// Primary Constructor.
 	// PRECONDITIONS:
-	//     _name is not an empty string
-	//     _number >= 0
+	//     number >= 0
 	// _name is set to name
 	// _number is set to number
 	ProductOrder(const std::string& name, int number);
